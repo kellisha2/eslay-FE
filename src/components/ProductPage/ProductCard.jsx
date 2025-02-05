01c9f0805bed6f6f26b5f878e6ed3ea7499b8ce7
kellisha2 <kellisha.andrew2@gmail.com>
2025-02-05 15:34:21 -0500

    sort by price filter added

--- src/components/ProductPage/ProductCard.jsx
+++ src/components/ProductPage/ProductCard.jsx
@@ -3,13 +3,20 @@ import { Link } from 'react-router-dom'
 
 function ProductCard({ product }) {
   return (
-    <div className='product'>
-      <div><Link to="/product/:productId">
-        <img className="product-img" src={product.img} alt={product.name} /></Link></div>
-        <div><h3 className='product-name'>{product.name}</h3></div>
+
+    <div>
+      <div className='product'>
+        <Link to="/product/:productId">
+        <img className="h-100" src={product.img} alt={product.name} /></Link>
+        <h3 className='product-name'>{product.name}</h3>
         <p className='product-price' >{product.price}</p>
-    
+
+      </div>
+
+
     </div>
+
+
   )
 }
 
--- src/components/ProductPage/ProductPage.jsx
+++ src/components/ProductPage/ProductPage.jsx
@@ -1,18 +1,32 @@
 import React, { useContext } from 'react'
 import ProductCard from './ProductCard'
 import { MyProductsContext } from '../../App'
+import Title from '../Homepage/Title'
 
 function ProductPage() {
   const { products } = useContext(MyProductsContext);
 
   return (
-    <div className='product-page'>
+    <div>
+      <div className='flex justify-between text-base sm:text-2x1 mb-4'>
+        <Title />
+
+      {/* Sort by Price */}
+        <select className='border-2 border-gray-300 text-sm px-3 '>
+          <option value="price">Sort by: Price</option>
+          <option value="img">Sort by: Low to High</option>
+          <option value="price">Sort by: High to Low</option>
+        </select>
+
+      </div>
+     <div className='grid grid-cols-4 gap-4 gap-y-6 h-200px'>
       <h1 >All Product</h1>
       {products &&
         products.map((product, index) => (
           <ProductCard product={product} key={index}/>
         ))
       }
+      </div>
     </div>
   )
 }
